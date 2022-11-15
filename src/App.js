91910deaa86f1c393f6de15b58ffb88bd87a7e2a
Corwin Hiatt <corwinhiatt@yahoo.com>
2022-11-15 13:57:32 -0500

    englis || spanish

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import { useState } from 'react';
 import Spanish from './lang/es.json'
 import English from './lang/en.json'
 import './App.css';
 
 const locale = navigator.language || 'en';
 
-const lang = (locale==='es') ? Spanish : English;
+const _lang = (locale==='es') ? Spanish : English;
 
 function App() {
+  const [lang, setLang] = useState(_lang);
+
   return (
     <div className="App">
       <header className="App-header">
+        <div>
+          <button onClick={() => setLang(English)}>EN</button>
+          <button onClick={() => setLang(Spanish)}>ES</button>
+        </div>
        <h1>{lang["app.header"]}</h1>
         <p>{lang["app.subhead"]}</p>
         <a
@@ -18,8 +25,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         {lang["app.cta"]}
         </a>
+        
       </header>
     </div>
   );
